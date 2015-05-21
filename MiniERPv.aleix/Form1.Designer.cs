fb9ac2161cc17da750e5a765f14c9be32490ebab
soler1212 <aleixsoler64@gmail.com>
2015-05-21 19:06:45 +0200

    Actualització 6
    He agregat el meu projecte

---
+++ MiniERPv.aleix/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace MiniERP
+{
+    partial class frmERP
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mnuMain = new System.Windows.Forms.MenuStrip();
+            this.arxiuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.articlesToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.proveïdorsToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.incorporarComandaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.recepcionarAlbaràToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.proveïdorsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.articlesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.articlesPendentsPerProveïdorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.valoracióStockToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.llistatsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.articlesToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.articlesPendentsDeRebreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.estocValoratToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.proveïdorsToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mnuMain
+            // 
+            this.mnuMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arxiuToolStripMenuItem,
+            this.llistatsToolStripMenuItem});
+            this.mnuMain.Location = new System.Drawing.Point(0, 0);
+            this.mnuMain.Name = "mnuMain";
+            this.mnuMain.Size = new System.Drawing.Size(451, 24);
+            this.mnuMain.TabIndex = 0;
+            this.mnuMain.Text = "menuStrip1";
+            // 
+            // arxiuToolStripMenuItem
+            // 
+            this.arxiuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.importarToolStripMenuItem,
+            this.exportarToolStripMenuItem});
+            this.arxiuToolStripMenuItem.Name = "arxiuToolStripMenuItem";
+            this.arxiuToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.arxiuToolStripMenuItem.Text = "Arxiu";
+            // 
+            // importarToolStripMenuItem
+            // 
+            this.importarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.articlesToolStripMenuItem1,
+            this.proveïdorsToolStripMenuItem1,
+            this.incorporarComandaToolStripMenuItem,
+            this.recepcionarAlbaràToolStripMenuItem});
+            this.importarToolStripMenuItem.Name = "importarToolStripMenuItem";
+            this.importarToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.importarToolStripMenuItem.Text = "Importar";
+            // 
+            // articlesToolStripMenuItem1
+            // 
+            this.articlesToolStripMenuItem1.Name = "articlesToolStripMenuItem1";
+            this.articlesToolStripMenuItem1.Size = new System.Drawing.Size(184, 22);
+            this.articlesToolStripMenuItem1.Text = "Articles";
+            this.articlesToolStripMenuItem1.Click += new System.EventHandler(this.articlesToolStripMenuItem1_Click);
+            // 
+            // proveïdorsToolStripMenuItem1
+            // 
+            this.proveïdorsToolStripMenuItem1.Name = "proveïdorsToolStripMenuItem1";
+            this.proveïdorsToolStripMenuItem1.Size = new System.Drawing.Size(184, 22);
+            this.proveïdorsToolStripMenuItem1.Text = "Proveïdors";
+            // 
+            // incorporarComandaToolStripMenuItem
+            // 
+            this.incorporarComandaToolStripMenuItem.Name = "incorporarComandaToolStripMenuItem";
+            this.incorporarComandaToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.incorporarComandaToolStripMenuItem.Text = "Incorporar Comanda";
+            // 
+            // recepcionarAlbaràToolStripMenuItem
+            // 
+            this.recepcionarAlbaràToolStripMenuItem.Name = "recepcionarAlbaràToolStripMenuItem";
+            this.recepcionarAlbaràToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.recepcionarAlbaràToolStripMenuItem.Text = "Recepcionar Albarà";
+            // 
+            // exportarToolStripMenuItem
+            // 
+            this.exportarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.proveïdorsToolStripMenuItem,
+            this.articlesToolStripMenuItem,
+            this.articlesPendentsPerProveïdorToolStripMenuItem,
+            this.valoracióStockToolStripMenuItem});
+            this.exportarToolStripMenuItem.Name = "exportarToolStripMenuItem";
+            this.exportarToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exportarToolStripMenuItem.Text = "Exportar";
+            this.exportarToolStripMenuItem.Click += new System.EventHandler(this.exportarToolStripMenuItem_Click);
+            // 
+            // proveïdorsToolStripMenuItem
+            // 
+            this.proveïdorsToolStripMenuItem.Name = "proveïdorsToolStripMenuItem";
+            this.proveïdorsToolStripMenuItem.Size = new System.Drawing.Size(239, 22);
+            this.proveïdorsToolStripMenuItem.Text = "Proveïdors";
+            // 
+            // articlesToolStripMenuItem
+            // 
+            this.articlesToolStripMenuItem.Name = "articlesToolStripMenuItem";
+            this.articlesToolStripMenuItem.Size = new System.Drawing.Size(239, 22);
+            this.articlesToolStripMenuItem.Text = "Articles";
+            // 
+            // articlesPendentsPerProveïdorToolStripMenuItem
+            // 
+            this.articlesPendentsPerProveïdorToolStripMenuItem.Name = "articlesPendentsPerProveïdorToolStripMenuItem";
+            this.articlesPendentsPerProveïdorToolStripMenuItem.Size = new System.Drawing.Size(239, 22);
+            this.articlesPendentsPerProveïdorToolStripMenuItem.Text = "Articles Pendents Per Proveïdor";
+            // 
+            // valoracióStockToolStripMenuItem
+            // 
+            this.valoracióStockToolStripMenuItem.Name = "valoracióStockToolStripMenuItem";
+            this.valoracióStockToolStripMenuItem.Size = new System.Drawing.Size(239, 22);
+            this.valoracióStockToolStripMenuItem.Text = "Valoració Stock";
+            this.valoracióStockToolStripMenuItem.Click += new System.EventHandler(this.valoracióStockToolStripMenuItem_Click);
+            // 
+            // llistatsToolStripMenuItem
+            // 
+            this.llistatsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.articlesToolStripMenuItem2,
+            this.articlesPendentsDeRebreToolStripMenuItem,
+            this.estocValoratToolStripMenuItem,
+            this.proveïdorsToolStripMenuItem2});
+            this.llistatsToolStripMenuItem.Name = "llistatsToolStripMenuItem";
+            this.llistatsToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.llistatsToolStripMenuItem.Text = "Llistats";
+            // 
+            // articlesToolStripMenuItem2
+            // 
+            this.articlesToolStripMenuItem2.Name = "articlesToolStripMenuItem2";
+            this.articlesToolStripMenuItem2.Size = new System.Drawing.Size(215, 22);
+            this.articlesToolStripMenuItem2.Text = "Articles";
+            // 
+            // articlesPendentsDeRebreToolStripMenuItem
+            // 
+            this.articlesPendentsDeRebreToolStripMenuItem.Name = "articlesPendentsDeRebreToolStripMenuItem";
+            this.articlesPendentsDeRebreToolStripMenuItem.Size = new System.Drawing.Size(215, 22);
+            this.articlesPendentsDeRebreToolStripMenuItem.Text = "Articles Pendents De Rebre";
+            // 
+            // estocValoratToolStripMenuItem
+            // 
+            this.estocValoratToolStripMenuItem.Name = "estocValoratToolStripMenuItem";
+            this.estocValoratToolStripMenuItem.Size = new System.Drawing.Size(215, 22);
+            this.estocValoratToolStripMenuItem.Text = "Estoc Valorat";
+            // 
+            // proveïdorsToolStripMenuItem2
+            // 
+            this.proveïdorsToolStripMenuItem2.Name = "proveïdorsToolStripMenuItem2";
+            this.proveïdorsToolStripMenuItem2.Size = new System.Drawing.Size(215, 22);
+            this.proveïdorsToolStripMenuItem2.Text = "Proveïdors";
+            // 
+            // frmERP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 173);
+            this.Controls.Add(this.mnuMain);
+            this.MainMenuStrip = this.mnuMain;
+            this.Name = "frmERP";
+            this.Text = "MINI ERP - BY DAM";
+            this.Load += new System.EventHandler(this.frmERP_Load);
+            this.mnuMain.ResumeLayout(false);
+            this.mnuMain.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mnuMain;
+        private System.Windows.Forms.ToolStripMenuItem arxiuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem articlesToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem proveïdorsToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem incorporarComandaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem proveïdorsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem articlesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem recepcionarAlbaràToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem llistatsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem articlesPendentsDeRebreToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem estocValoratToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem proveïdorsToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem articlesPendentsPerProveïdorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem valoracióStockToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem articlesToolStripMenuItem2;
+    }
+}
+
---
+++ MiniERPv.aleix/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.Odbc;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.XPath;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
+namespace MiniERP
+{
+    public partial class frmERP : Form
+    {
+        public const string CONNECTIONSTRING = "Driver={Microsoft Access Driver (*.mdb)};DBQ=minierp.mdb";
+
+        private OdbcConnection cn;
+        private OdbcDataAdapter da;
+        DataSet ds;
+
+        public frmERP()
+        {
+            cn = new OdbcConnection(CONNECTIONSTRING);
+            cn.Open();
+            InitializeComponent();
+        }
+
+        //ACCIONS
+        private void frmERP_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exportarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void valoracióStockToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+         
+        
+
+        private void articlesToolStripMenuItem1_Click(object sender, EventArgs e)
+        {        
+            string connectionString;
+
+            //Extreiem del XML:
+            string codi = "";
+            string descripcio = "";
+            int preu = 0;
+            int estoc = 0;
+
+            //Validem XML
+            ValidarXML("articles", "articles");
+
+            //connectem amn la base de dades
+            connectionString = "Driver={Microsoft Access Driver (*.mdb)};DBQ=minierp.mdb";
+
+            OdbcConnection ConnexioMySQL = new OdbcConnection(connectionString);
+
+            ConnexioMySQL.Open();
+
+            //Seleccionem les dades
+
+            XmlDocument xml = new XmlDocument();
+
+            xml.Load("articles.xml");
+
+            XmlNodeList xnList = xml.SelectNodes("/articles/article");
+
+            foreach (XmlNode xn in xnList)
+
+            {
+                try
+                {
+                codi = xn["codi"].InnerText;
+
+                descripcio = xn["descripcio"].InnerText;
+
+                estoc = Convert.ToInt32(xn["estoc"].InnerText);
+
+                preu = Convert.ToInt32(xn["preu"].InnerText);
+
+                // PER FER UN INSERT O UPDATE
+                
+                OdbcCommand cmd = new OdbcCommand();
+
+                cmd.Connection = ConnexioMySQL;
+
+                cmd.CommandText = "INSERT INTO article VALUES ('" + codi + "','" + descripcio + "','" + estoc + "','" + preu + "');";
+
+                cmd.ExecuteNonQuery();
+                }
+                catch (Exception g)
+                {
+                    AfegirError("L'ARTICLE AMB CODI: "+codi + " JA EXISTEIX A LA TAULA MESTRE D'ARTICLES", "IMPORTAR ARTICLES");
+
+                }
+
+            }
+
+            ConnexioMySQL.Close();
+            
+
+        }
+        //FALTA TREURE ELS ERRORS DEL SISTEMA PODRIA SER UNA FUNCIO PER PARAR EL PROGRAMA SI ES TRUE;
+        private void ValidarXML(string xml, string xsd)
+        {
+            try
+            {
+                bool isValid = false;
+                XmlReaderSettings settings = new XmlReaderSettings();
+                settings.Schemas.Add(null, xsd + ".xsd");
+                settings.ValidationType = ValidationType.Schema;
+                XmlDocument document = new XmlDocument();
+                document.Load(xml + ".xml");
+                XmlReader rdr = XmlReader.Create(new StringReader(document.InnerXml), settings);
+
+                while (rdr.Read())
+                {
+
+                }
+                isValid = true;
+            }
+
+            catch (Exception e)
+            {
+                MessageBox.Show("El fitxer XML no és correcte d'acord amb les normes del fitxer XSD", "ERROR");
+                
+            }
+
+        }
+
+
+        private void crearFitxerError()
+        {
+            StreamWriter arxiu = new StreamWriter("errors.xml", false);
+
+            arxiu.WriteLine("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+
+            arxiu.WriteLine("<errors>");
+
+            arxiu.WriteLine("</errors>");
+
+            arxiu.Close();
+        }
+
+        //PREGUNTAR SI ES CORRECTE
+        
+        private void AfegirError(string descripcio, string proces)
+        {
+
+            try
+            {
+                //CARREGUEM EN MEMÒRIA EL FITXER D'ERRORS
+
+                XmlDocument doc = new XmlDocument();
+
+                doc.Load("errors.xml");
+
+                //root representa l'arrel del document: <errors>
+
+                XmlNode root = doc.DocumentElement;
+
+                //CREEM UN NOU NODE D'ERROR  <error></error>
+
+                XmlElement elem = doc.CreateElement("error");
+
+                //CREEM SUBNODES FILL DE L'ERROR, TANTS COM NECESSITEM PER PRECISSAR 
+                //DETALLS DE L'ERROR: DATA, PROCES QUE L'HA GENERAT, DESCRIPCIÓ ...
+
+                XmlElement subElement1 = doc.CreateElement("descripcio");
+
+                subElement1.InnerText = descripcio;
+
+                XmlElement subElement2 = doc.CreateElement("proces");
+
+                subElement2.InnerText = proces;
+
+                //AFEGIM ELS SUBELEMENTS COM A FILLS DE l'ETIQUETA <error>
+
+                elem.AppendChild(subElement1);
+
+                elem.AppendChild(subElement2);
+
+                //AFEGIM el nou error com a nou fill de l'arrel <errors>
+
+                root.AppendChild(elem);
+
+                //ACTUALITZEM EL DOCUMENT A DISC (FINS ARA TOT ES FEIA A MEMÒRIA)
+
+                doc.Save("errors.xml");
+            }
+            catch (Exception h)
+            {
+                StreamWriter arxiu = new StreamWriter("errors.xml", false);
+
+                arxiu.WriteLine("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+
+                arxiu.WriteLine("<errors>");
+
+                arxiu.WriteLine("</errors>");
+
+                arxiu.Close();
+            }
+        }
+        
+   }
+}
+
